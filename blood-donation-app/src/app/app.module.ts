db6fabe5834941535818f0a704ee8b53df95e5c7
SladjanaColakovic <colakovic.ra32.2018@uns.ac.rs>
2023-08-29 13:22:57 +0200

    homepage

--- blood-donation-app/src/app/app.module.ts
+++ blood-donation-app/src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomePageComponent } from './homepage/home-page/home-page.component';
+import { BloodDonationInformationComponent } from './homepage/blood-donation-information/blood-donation-information.component';
+import { BloodDonationGaleryComponent } from './homepage/blood-donation-galery/blood-donation-galery.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavBarComponent,
+    HomePageComponent,
+    BloodDonationInformationComponent,
+    BloodDonationGaleryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
   ],
   providers: [],
   bootstrap: [AppComponent]
